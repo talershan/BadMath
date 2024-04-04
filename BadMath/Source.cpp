35f5ba22a434f57b16918f2f94f42d62b08a8572
talershan <melificon@gmail.com>
2024-04-04 15:31:37 -0400

    Fixed bugs, made ints floats so user can enter decimals, and added precision so user gets a more precise answer and the decimals work better

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,23 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
+float average(float i1, float i2, float i3)//changed ints to floats to allow the user to enter decimals
 {
-	return i1 + i2 + i3 / 3.0f;
+	return (i1 + i2 + i3) / 3.0f;//can't forget the parenthesis  
 }
 
 int main()
 {
-	int n1 =0 , n2 = 0, n3 = 0;
+	float n1 = 0, n2 = 0, n3 = 0, a;//changed to float for decimals and added a for easier organizing
+
 	cout << "This program calculates the average of three numbers." << endl;
 	cout << "First number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;//fixed this
 
-	float a = average(n1, n2, n3);
+	a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(2) << fixed << "The average is " << a << endl; //added more precision since it's a float and the user can enter decimals now
 
 	return 0;
 }
